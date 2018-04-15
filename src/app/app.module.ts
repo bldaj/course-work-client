fa4744af1ed56e1d3278a8b6e0dbb2af532c7708
k <wetarmor@gmail.com>
2018-04-15 19:45:23 +0300

    added angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { MaterialAppModule } from './ngmaterial.module';
 
 import { ServerService } from './server.service';
 import { HttpClientModule } from '@angular/common/http';
@@ -19,8 +21,10 @@ import { CveYearDetailComponent } from './cve-year-detail/cve-year-detail.compon
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    MaterialAppModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [ ServerService ],
   bootstrap: [AppComponent]
