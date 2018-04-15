8a014e9a8a5c805270b5cbe1e1e2b230c8e0e957
k <wetarmor@gmail.com>
2018-04-15 21:27:28 +0300

    changed style for cves list

--- src/app/ngmaterial.module.ts
+++ src/app/ngmaterial.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
 
 
 @NgModule({
-  imports: [ MatButtonModule, MatToolbarModule ],
-  exports: [ MatButtonModule, MatToolbarModule ]
+  imports: [ MatButtonModule, MatToolbarModule, MatListModule ],
+  exports: [ MatButtonModule, MatToolbarModule, MatListModule ]
 })
 export class MaterialAppModule {}
